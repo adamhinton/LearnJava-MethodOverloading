35adc39af1824f7d1491a0f2293dc34126fb490d
adamd <adam.d.hinton@gmail.com>
2024-02-13 11:19:42 -0500

    starting on overloading

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
 public class Main {
 
+    public static void main(String[] args) {
+        int newScore = calculateScore("Adam", 500);
+        System.out.println(newScore);
+    }
+
+    public static int calculateScore(String playerName, int score){
+        System.out.println(playerName + score);
+        return score * 1000;
+    }
+
+
 }
