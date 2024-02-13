e860b1d8446b506aa93c8ce30ccd75308717296e
adamd <adam.d.hinton@gmail.com>
2024-02-13 11:28:58 -0500

    exploring stuff that reminds me of recursion

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        int newScore = calculateScore("Adam", 500);
-        System.out.println(newScore);
-
-        calculateScore(75);
-
-        calculateScore();
+        System.out.println("New score is " + calculateScore("Adam", 500));
     }
 
     public static int calculateScore(String playerName, int score){
-        System.out.println(playerName + score);
         return score * 1000;
     }
 
     public static int calculateScore(int score){
-        System.out.println(score);
-        return score * 1000;
+        return calculateScore("Anonymous", score);
     }
 
     public static int calculateScore(){
         System.out.println("No player name, no player score");
         return 0;
     }
+
 
 }
